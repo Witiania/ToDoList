d947ed3ef47a6e1706a1d787413fe92a9be81b67
Witiania <Witiania@gmail.com>
2023-01-20 18:23:24 +0300

    Создал Адаптер (CustomAdapter)

---
+++ app/src/main/java/com/example/todolist/CustomAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.todolist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter(private val mList: List<ToDoItem>):RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = mList[position]
+        holder.title.text = item.title
+        holder.description.text = item.description
+        holder.number.text = item.number.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val title: TextView = itemView.findViewById(R.id.item_recycler_title)
+        val description: TextView = itemView.findViewById(R.id.item_recycler_description)
+        val number: TextView = itemView.findViewById(R.id.item_recycler_number)
+
+    }
+}
