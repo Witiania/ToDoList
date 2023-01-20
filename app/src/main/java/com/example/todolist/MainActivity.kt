4f5a6d4e8607d4bdfd519e9bc18ff2229ac1137d
Witiania <Witiania@gmail.com>
2023-01-20 19:07:02 +0300

    Инициализировали и подключили адаптер в MainActivity

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(R.id.main_recycler_view)
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        //Создаем проверку нашего Адаптера, используя цикл
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ToDoItem>()
+
+        // This loop will create 100 Views containing
+        for (item in 1..100) {
+            data.add(ToDoItem("title", "description",item))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
+
     }
 }
--- app/src/main/java/com/example/todolist/ToDoItem.kt
+++ app/src/main/java/com/example/todolist/ToDoItem.kt
@@ -3,5 +3,5 @@ package com.example.todolist
 data class ToDoItem(
     val title:String,
     val description:String,
-    val number:Int
+    val number: Int
 )
